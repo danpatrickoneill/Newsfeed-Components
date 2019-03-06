d2b53f105a2aef78ea8cdbf5f5bb4970ae6fd5e0
danpatrickoneill <dan47573@gmail.com>
2019-03-06 17:21:51 -0500

    Added animated expand/collapse and close button to articles

--- Article/Article.js
+++ Article/Article.js
@@ -6,16 +6,34 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelectorAll('.expandButton')[0];
+    this.closeButton = domElement.querySelectorAll('.closeButton')[0];
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand"
+    this.expandButton.textContent = "Click to Expand";
+    this.closeButton.textContent = "X";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle())
+    this.expandButton.addEventListener('dblclick', () => this.contractArticle())
+
+    this.closeButton.addEventListener('click', () => this.closeArticle())
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     console.log("Clicking the span");
-    this.domElement.classList.toggle('article-open');
+    TweenMax.to(this.domElement, .5, {className: "+=article-open"})
+    this.expandButton.textContent = 'Double-Click to Collapse';
+  }
+
+  contractArticle() {
+    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    console.log("Clicking the span");
+    TweenMax.to(this.domElement, .5, {className: "-=article-open"});
+    this.expandButton.textContent = 'Click to Expand';
+  }
+
+  closeArticle() {
+    // this.domElement.style.display = 'none'
+    TweenMax.to(this.domElement, .5, {display: 'none', opacity: 0})
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,6 +9,6 @@ const menu = document.getElementsByClassName('menu')[0];
 
 // create a reference to the ".menu-button" class
 const menuButton = document.getElementsByClassName('menu-button')[0];
-
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu)
+menuButton.addEventListener('click', toggleMenu);
+
